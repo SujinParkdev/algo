999d244cb2ca0172996745620459b39875973a82
SujinParkdev <sujin.park.dev@gmail.com>
2018-06-06 17:23:51 +0900

    Make Even Or Odd Function

---
+++ algorithm_fds/180518-2.js
@@ -0,0 +1,11 @@
+// javascript를 활용해서 입력받은 문자를 역순으로 반환하는 함수를 만들어라.
+
+function reverse(str) {
+  // 이 곳에 코드를 작성해 주세요.
+  }
+  
+  // 실행을 위한 테스트코드입니다.
+  console.log(reverse('hello'));
+  
+  // 출력 예시
+  // olleh
---
+++ algorithm_fds/180518-3.js
@@ -0,0 +1,11 @@
+// 첫 번째와 마지막 글자를 제외하고 출력하시오.
+
+function middleCharacter(str){
+  // 이 곳에 코드를 작성해 주세요.
+}
+
+// 실행을 위한 테스트코드입니다.
+console.log(middleCharacter('thellot'));
+
+// 출력 예시
+// hello
